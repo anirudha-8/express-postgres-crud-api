038c419c8243a030991b7720dcb66306be5d87f5
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-20 10:53:22 +0530

    feat(src/index.js): create express basic server

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors);
+
+// ROUTES
+
+// ERROR HANDLING MIDDLEWARE
+
+// RUNNING SERVER
+app.listen(PORT, () => {
+	console.log(`Server is running on - http://localhost:${PORT}`);
+});
