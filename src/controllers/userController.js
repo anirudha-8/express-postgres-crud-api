e6fe3e3363c4e85dd2eab4f2724e6a484d215b89
Anirudha Bele <beleanirudha8@gmail.com>
2026-01-26 17:45:48 +0530

    feat(src/controllers/userController.js): create user controllers strucutre

---
+++ src/controllers/userController.js
@@ -0,0 +1,54 @@
+const getAllUser = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error! Please, try again!",
+		});
+	}
+};
+
+const getUserById = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error! Please, try again!",
+		});
+	}
+};
+
+const createUser = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error! Please, try again!",
+		});
+	}
+};
+
+const updateUser = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error! Please, try again!",
+		});
+	}
+};
+
+const deleteUser = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal Server Error! Please, try again!",
+		});
+	}
+};
